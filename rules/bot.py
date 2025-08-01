744ee2b01260f136998d235ccc7203ab9564889a
rif av <reef.abramson@gmail.com>
2025-08-01 23:24:59 +0300

    first commit

---
+++ rules/bot.py
@@ -0,0 +1,41 @@
+from keep_alive import app
+from discord.ext import commands
+import discord
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def rule(ctx: commands.Context, number: int):
+    guild = ctx.guild
+    rules_channel = guild.rules_channel
+
+    if rules_channel is None:
+        rules_channel = discord.utils.get(guild.text_channels, name="rules")
+
+    if not rules_channel:
+        await ctx.send("Rules channel not found.")
+        return
+
+    async for message in rules_channel.history(limit=100):
+        content = message.content.strip()
+        
+        if content.startswith(f"{number}"):
+            await ctx.send(f"**Rule {number}:**\n`{content[len(f"{number}"):]}`")
+            return
+
+        elif content.lower().startswith(f"rule {number}"):
+            await ctx.send(f"**Rule {number}:**\n`{content[len(f"rule {number}"):].strip()}`")
+            return
+
+    await ctx.send(f"Rule {number} not found in {rules_channel.mention}.")
+
+bot.run(TOKEN)
---
+++ rules/keep_alive.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
